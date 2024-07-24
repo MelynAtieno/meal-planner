6842948cac8e17c43563af053768868bd0aabd48
Melyn Were <WereMA@cardiff.ac.uk>
2024-07-25 00:24:40 +0100

    increase margin between links

--- Navbar.js
+++ Navbar.js
@@ -6,3 +6,16 @@ function myFunction(){
         x.className = "topnav";
     }
 }
+
+document.addEventListener("DOMContentLoaded",
+    function(){
+        const navItems = document.querySelectorAll("a");
+
+        navItems.forEach(item => {
+            item.addEventListener("click",function(){
+                navItems.forEach(navItem => navItem.classList.remove("active"));
+                this.classList.add("active");
+            });
+        });
+    }
+);
