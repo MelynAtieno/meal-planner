29670f77b505abac7a552ca2e2d64b188ef2f2c5
Melyn Were <WereMA@cardiff.ac.uk>
2024-08-13 16:10:33 +0100

    finish search function

--- Home.js
+++ Home.js
@@ -1,8 +1,9 @@
 const api_url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=d892684e&app_key=56901bab6e1988ad6ca75aabf25b2bda%09&imageSize=SMALL&random=true&field=label&field=image&field=url&field=ingredientLines&field=calories&field=yield";
+const searchButton = document.getElementById("btn-search");
 const outputElement = document.getElementById("display-recs");
 const savedElement = document.getElementById("saved-sec");
-const searchBar = document.getElementById("searchBar");
-const searchButton = document.getElementById("btn-search")
+
+
 
 fetch(api_url)
     .then(response =>{
@@ -35,43 +36,39 @@ fetch(api_url)
 
 //search for a recipe
 function searchRecipes(){
-   // e.preventDefault();
-    if(searchBar.value !== ''){
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=d892684e&app_key=56901bab6e1988ad6ca75aabf25b2bda%09&imageSize=SMALL&field=label&field=image&field=url&field=ingredientLines&field=calories&field=yield&q=${searchBar.value}`)
+    const searchBar = document.getElementById("search-input");
+    const input = searchBar.value;
+    const search_url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=d892684e&app_key=56901bab6e1988ad6ca75aabf25b2bda%09&imageSize=SMALL&field=label&field=image&field=url&field=ingredientLines&field=calories&field=yield&q=${input}`;
+    const outputElement = document.getElementById("display-recs");
+    
+    if(input !== ''){
+    fetch(search_url)
     .then(response => response.json())
-    .then(data =>{data.hits.forEach(data => {        
+    .then(data =>{
+        outputElement.innerHTML = '';
+        data.hits.forEach(recipeData => {        
         outputElement.innerHTML +=   `
                                     <div class=recipes>    
-                                    <h3>${data.recipe.label}</h3>
-                                 <img src="${data.recipe.image}"></img>
-                                 <p><b>Calories:</b> ${Math.round(data.recipe.calories)} </p>
-                                 <p><b>Servings:</b> ${data.recipe.yield}</p>
+                                    <h3>${recipeData.recipe.label}</h3>
+                                 <img src="${recipeData.recipe.image}"></img>
+                                 <p><b>Calories:</b> ${Math.round(recipeData.recipe.calories)} </p>
+                                 <p><b>Servings:</b> ${recipeData.recipe.yield}</p>
                                  <p class=ingredients><b>Ingredients</b></p>
-                                 <p>${data.recipe.ingredientLines}</p>
-                                 <span><button><a href="${data.recipe.url}" target="_blank"><b>RECIPE</b></a></button></span> <span><button class=btn-save><b>SAVE</b></button></span>
+                                 <p>${recipeData.recipe.ingredientLines}</p>
+                                 <span><button><a href="${recipeData.recipe.url}" target="_blank"><b>RECIPE</b></a></button></span> <span><button class=btn-save><b>SAVE</b></button></span>
                                  </div>`;
 
     })
 })
+.catch(error => {
+    console.error("Error fetching recipes", error);
+});
     } else {
         alert("Please enter a value")
     }
+    
 }
 
 
 //save recipe
-    $(document).on("click", ".btn-save", function(e){
-        e.preventDefault();
-
-    
-    let savedRecipes = outputElement.value;
-    localStorage.setItem("recipes", savedRecipes);
-    displayData();
-});
-    function displayData(){
-    const savedData = localStorage.getItem("recipes");
-    savedElement.innerHTML = savedData;
- }
-    displayData();
-
-
+   
